fc9a6218c0ed3bc5384eba0034fe1015fa430087
Batmanghelich, Farhad <feb.lich@gmail.com>
2022-04-04 16:04:25 -0700

    initial commit, added skeleton for predictive model objects

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import Lasso
+
+class SuperGenesLasso:
+    pass
 
 st.write("Hello World test connection")
