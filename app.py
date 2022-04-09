e4a523fcfe369ccb401e2a6b9db06009c8bb684d
ms4738 <monicasingh4738@gmail.com>
2022-04-08 21:23:37 -0500

    testing

--- app.py
+++ app.py
@@ -1,3 +1,40 @@
 import streamlit as st
+import streamlit_authenticator as stauth
+import psycopg2
+from urllib.parse import urlparse
+import os
 
-st.write("Hello World test connection")
+result = urlparse("postgresql://postgres:postgres@localhost/postgres")
+username = result.username
+password = result.password
+database = result.path[1:]
+hostname = result.hostname
+port = result.port
+connection = psycopg2.connect(
+    database = database,
+    user = username,
+    password = password,
+    host = hostname,
+    port = port
+)
+st.write(os.environ.get('DATABASE_URL'))
+
+names = ['John Smith','Rebecca Briggs']
+usernames = ['jsmith','rbriggs']
+passwords = ['123','456']
+
+hashed_passwords = stauth.Hasher(passwords).generate()
+
+authenticator = stauth.Authenticate(names,usernames,hashed_passwords,
+    'some_cookie_name','some_signature_key',cookie_expiry_days=30)
+
+name, authentication_status, username = authenticator.login('Login','main')
+
+if authentication_status:
+    st.write('Welcome *%s*' % (name))
+    st.title('Some content')
+elif authentication_status == False:
+    st.error('Username/password is incorrect')
+elif authentication_status == None:
+    st.warning('Please enter your username and password')
+
