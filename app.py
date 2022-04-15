fb4fd0f54a1304ee1d8e0d34cae3f420ec556961
ms4738 <monicasingh4738@gmail.com>
2022-04-14 20:09:15 -0500

    Integrated visualizations

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ import pickle
 from predictive_models import SuperGenes
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
+import plotly.express as px
+import plotly.subplots as sp
 
 connection = DBUtility.init_connection()
 user_credentials = DBUtility.run_query(connection,query="SELECT * from user_credentials;")
@@ -30,8 +33,25 @@ if authentication_status:
 
     ##TODO upload cell line data for patient to DB
     test_df = pd.read_csv(uploaded_file)
-    super_genes_model = pickle.load(open('model.pkl', 'rb'))
-    y_pred = super_genes_model.predict(test_df)
+
+    super_genes_models = pickle.load(open('model.pkl', 'rb'))
+    # st.write(super_genes_models)
+    pred_arr = np.empty([test_df.shape[0], len(super_genes_models.keys())])
+    pred_arr[:] = np.nan
+    for i, drug in enumerate(super_genes_models.keys()):
+        y_pred = super_genes_models[drug].predict(test_df)
+        pred_arr[:, i] = y_pred
+
+    pred_arr = pd.DataFrame(pred_arr, columns=[drug for drug in super_genes_models.keys()])
+    pred_arr['sample_number'] = ['sample ' + str(num) for num in range(pred_arr.shape[0])]
+    pred_arr.set_index('sample_number', inplace=True)
+
+    st.title("IC50 prediction")
+
+    pred_arr = pred_arr.stack().reset_index().rename(columns={0:'IC50', 'level_1': 'Drug'})
+
+
+    # st.write(pred_arr)
 
 
 
@@ -42,6 +62,8 @@ if authentication_status:
     #TODO store them in the database (PATIENT ID, TREATMENT, IC50 VALUE)
 
     #TODO visualization --- select db
+
+
     drug_rslts = pd.read_csv("data/Drug_sensitivity_IC50_(Sanger_GDSC1).csv")
     drug_rslts = drug_rslts.fillna(0)
     drug_rslts.set_index("Unnamed: 0", inplace=True)
@@ -50,31 +72,149 @@ if authentication_status:
                        'value': 'IC50'}, inplace=True)
     df = df.loc[(df != 0).all(axis=1)]
 
-    # st.set_page_config(layout="wide")
-    st.title("Interact with Drug_sensitivity_IC50_(Sanger_GDSC2)")
+    # st.write(df)
 
-    drug_list = ['mitomycin-C (GDSC1:136)']
+    # st.set_page_config(layout="wide")
+    # st.title("Interact with Drug_sensitivity_IC50_(Sanger_GDSC2)")
+
+    drug_list = [drug for drug in super_genes_models.keys()]
+    # patient_list = list(df['Patient'].unique())
+    samples = list(pred_arr['sample_number'].unique())
 
     with st.sidebar:
         st.subheader("Configure the plot")
-        drug = st.selectbox(label="Choose a drug", options=drug_list)
+        patient = st.selectbox(label="Choose a patient", options=samples)
 
-    query_1 = f"Drug=='{drug}'"
-    df_filtered_1 = df.query(query_1)
+        chart_visual = st.sidebar.selectbox('Select Chart',
+                                            ('Predictions', 'Sanger GDSC2', 'Model Metrics'))
+    query_1 = f"sample_number=='{patient}'"
+    df_patient = pred_arr.query(query_1)
 
+    # st.write(df_patient)
 
-    sns.set(rc={'figure.figsize': (11.7, 8.27)})
-    sns.displot(df_filtered_1, x="IC50", kind="kde", bw_adjust=2, fill=True)
-    plt.axvline(x=df_filtered_1.IC50.mean(),
-                color='blue',
-                ls='--',
-                lw=2.5)
-    plt.axvline(x=y_pred[id],
-                color='red',
-                ls='-',
-                lw=2.5)
+    # drug_subset = ['cisplatin (GDSC2:1005)',
+    #                'docetaxel (GDSC2:1007)',
+    #                'gefitinib (GDSC2:1010)',
+    #                'gemcitabine (GDSC2:1190)',
+    #                'paclitaxel (GDSC2:1080)']
 
-    st.pyplot(plt.gcf())
+    if chart_visual == 'Predictions':
+
+        with st.sidebar:
+            treatment_select = st.multiselect(label="Pick Treatment(s)", options=drug_list)
+
+        # if len(treatment_select) == 0:
+        #
+        #     f = plt.figure(figsize=(10, 15))
+        #     for i, val in enumerate(drug_list):
+        #         # f.add_subplot(len(drug_list), int(len(drug_list)/2), i + 1)
+        #         sns.distplot(df['IC50'][df['Drug'] == val], hist=True)
+        #
+        #         plt.axvline(x=df['IC50'][df['Drug'] == val].mean(),
+        #                     color='blue',
+        #                     ls='--',
+        #                     lw=2.5,
+        #                     label="Average IC50")
+        #
+        #         if len(df_patient[df_patient['Drug'] == val]['IC50']) != 0:
+        #
+        #             treatment_avg = df['IC50'][df['Drug'] == val].mean()
+        #             patient_result = df_patient[df_patient['Drug'] == val]['IC50'].values[0]
+        #
+        #             if patient_result > treatment_avg:
+        #                 linecolor = 'green'
+        #             else:
+        #                 linecolor = 'red'
+        #
+        #             plt.axvline(x=df_patient[df_patient['Drug'] == val]['IC50'].values[0],
+        #                         color=linecolor,
+        #                         ls='-',
+        #                         lw=2.5,
+        #                         label="Patient's IC50")
+        #
+        #         plt.title(str(df['Drug'][df['Drug'] == val].iloc[0]), fontweight='bold')
+        #         plt.legend()
+        #         plt.grid()
+        #         plt.tight_layout()
+        #
+        #     st.pyplot(f)
+
+        # else:
+
+        if (len(treatment_select) > 0):
+
+            f = plt.figure(figsize=(10, 15))
+            for i, val in enumerate(treatment_select):
+                f.add_subplot(4, 2, i + 1)
+                sns.distplot(df['IC50'][df['Drug'] == val], hist=True)
+
+                plt.axvline(x=df['IC50'][df['Drug'] == val].mean(),
+                            color='blue',
+                            ls='--',
+                            lw=2.5,
+                            label="Average IC50")
+
+                if len(df_patient[df_patient['Drug'] == val]['IC50']) != 0:
+
+                    treatment_avg = df['IC50'][df['Drug'] == val].mean()
+                    patient_result = df_patient[df_patient['Drug'] == val]['IC50'].values[0]
+
+                    if patient_result > treatment_avg:
+                        linecolor = 'green'
+                    else:
+                        linecolor = 'red'
+
+                    plt.axvline(x=df_patient[df_patient['Drug'] == val]['IC50'].values[0],
+                                color=linecolor,
+                                ls='-',
+                                lw=2.5,
+                                label="Patient's IC50")
+
+                plt.title(str(df['Drug'][df['Drug'] == val].iloc[0]), fontweight='bold')
+                plt.legend()
+                plt.grid()
+                plt.tight_layout()
+
+            st.pyplot(f)
+
+    if chart_visual == 'Sanger GDSC2':
+        with st.sidebar:
+            patient = st.multiselect(label="Pick Treatment(s)", options=drug_list)
+
+        df.rename(columns={"Drug": "Treatment(s)"}, inplace=True)
+
+        if len(patient) == 0:
+            fig = px.violin(df[df['Treatment(s)'].isin(drug_list)], y='IC50', x='Treatment(s)',
+                            color='Treatment(s)', box=True,
+                            hover_data=df[df['Treatment(s)'].isin(drug_list)].columns,
+                            width=1000, height=700)
+            fig.update_yaxes(title='IC50 (micromolar)')
+            fig.update_xaxes(title=None, showticklabels=False)
+            st.plotly_chart(fig, sharing="streamlit")
+
+        if len(patient) > 0:
+            fig = px.violin(df[df['Treatment(s)'].isin(patient)], y='IC50', x='Treatment(s)',
+                            color='Treatment(s)', box=True,
+                            hover_data=df[df['Treatment(s)'].isin(patient)].columns,
+                            width=1000, height=700)
+            fig.update_yaxes(title='IC50 (micromolar)')
+            fig.update_xaxes(title=None, showticklabels=False)
+            st.plotly_chart(fig)
+
+    if chart_visual == 'Model Metrics':
+
+        #### Model Performance Metrics ####
+        metric = ['accuracy', 'accuracy', 'rmse', 'rmse']
+        split = ['Training Data', 'Test Data', 'Training Data', 'Test Data']
+        vals = [0.8234, 0.71234, 0.2435, 0.123]
+
+        metrics = pd.DataFrame(list(zip(metric, split, vals)), columns=['Metric', 'Split', 'Value'])
+        print("metrics:", metrics)
+
+        pm_fig = px.histogram(metrics, x='Split', y='Value', facet_col='Metric', color='Split',
+                              title='Model Performance Metrics')
+        pm_fig.update_layout(showlegend=False)
+        st.plotly_chart(pm_fig, sharing="streamlit")
 
 elif authentication_status == False:
     st.error('Username/password is incorrect')
--- predictive_models.py
+++ predictive_models.py
@@ -12,6 +12,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import mean_squared_error
 import pickle
+from collections import defaultdict
 
 class SuperGenes:
     def __init__(self, clustering_method, model_config):
@@ -25,9 +26,6 @@ class SuperGenes:
     def _cluster(self, X):
         if self.clustering_method == 'hierarchical_clustering':
             X = X.T
-            # self.scaler = StandardScaler()
-            # scaled_X = self.scaler.fit_transform(X)
-            # X = pd.DataFrame(scaled_X, index=X.index, columns=X.columns)
             cluster_obj = AgglomerativeClustering(n_clusters=500, compute_distances=True)
             cluster_obj.fit(X)
             self.super_genes_labels = cluster_obj.labels_
@@ -43,52 +41,59 @@ class SuperGenes:
 
     def predict(self, X):
         X = X.T
-        # X = pd.DataFrame(self.scaler.transform(X), index=X.index, columns=X.columns)
         X['cluster_labels'] = self.super_genes_labels
         X = X.groupby(by="cluster_labels").mean()
         X = X.T
         return self.mdl.predict(X)
 
+def create_training_set(gene_exp, drug_IC50, drug_col_name):
+    # drug_col_name = drug_IC50.columns[drug_IC50.columns.str.contains(drug_name)][0]
+    drug_name_df = drug_IC50[["Unnamed: 0", drug_col_name]]
+    drug_name_df.dropna(inplace=True)
+    gene_exp_IC50 = gene_exp.merge(drug_name_df)
+    # drug_col_name = gene_exp_IC50.columns[gene_exp_IC50.columns.str.contains(drug_name)][0]
+    X = gene_exp_IC50.loc[:, gene_exp_IC50.columns != drug_col_name]  # features
+    X = X.loc[:, X.columns != 'Unnamed: 0']  # features
+    y = gene_exp_IC50[drug_col_name]
+    return X, y
 
 if __name__ == "__main__":
-
-
+    # super_genes_models = pickle.load(open('model.pkl', 'rb'))
+    # print(super_genes_models)
 
     ## read in gene expression data
     gene_exp = pd.read_csv("data\Expression_22Q1_Public.csv")
     gene_exp.dropna(inplace=True)
 
     ## read in drug IC50 data
-    drug_IC50 = pd.read_csv("data\Drug_sensitivity_IC50_Sanger_GDSC1.csv")
-    mitomycin = drug_IC50[["Unnamed: 0", "mitomycin-C (GDSC1:136)"]]
-    mitomycin.dropna(inplace=True)
-    trametinib = drug_IC50[["Unnamed: 0", "trametinib (GDSC1:1372)"]]
-    trametinib.dropna(inplace=True)
+    all_drugs_IC50 = pd.read_csv("data\Drug_sensitivity_IC50_Sanger_GDSC1.csv")
 
-    ## merge the gene epression and IC50 for drugs
-    data_mitomycin = gene_exp.merge(mitomycin)
-    data_trametinib = gene_exp.merge(trametinib)
+    ## choose the 20 drugs that have the least nan
+    frequent_drugs = all_drugs_IC50.isna().sum().sort_values(ascending=True)[:21]
+    frequent_drugs.drop('Unnamed: 0', inplace=True)
 
-    X = data_mitomycin.loc[:, data_mitomycin.columns != 'mitomycin-C (GDSC1:136)']  # features
-    X = X.loc[:, X.columns != 'Unnamed: 0']  # features
-    y = data_mitomycin['mitomycin-C (GDSC1:136)']  # response Variable
+    # list of drug to the analysis
+    drug_list = [drug for drug in frequent_drugs.index]
+    models_dict = defaultdict()
+    RMSEs = []
+    for drug in drug_list:
 
-    # X = data_trametinib.loc[:, data_trametinib.columns != 'trametinib (GDSC1:1372)']  # features
-    # X = X.loc[:, X.columns != 'Unnamed: 0']  # features
-    # y = data_trametinib['trametinib (GDSC1:1372)']  # response Variable
+        X, y = create_training_set(gene_exp, all_drugs_IC50, drug)
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3,
-                                                        random_state=42)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3,
+                                                            random_state=42)
+        mdl = SuperGenes(clustering_method='hierarchical_clustering',
+                         model_config={'name': Lasso(), 'parameters': {'alpha': [.0000001, .0001]}})
 
-    mdl = SuperGenes(clustering_method='hierarchical_clustering',
-                     model_config={'name': Lasso(), 'parameters': {'alpha': [.0000001, .0001]}})
+        mdl.learning(X_train, y_train)
+        models_dict[drug] = mdl
+        y_pred = mdl.predict(X_test)
+        rmse = mean_squared_error(y_test, y_pred)
+        RMSEs.append(rmse)
 
-    mdl.learning(X_train, y_train)
-    y_pred = mdl.predict(X_test)
-    rmse = mean_squared_error(y_test, y_pred)
-    pickle.dump(mdl, open('model.pkl', 'wb'))
-
-
+    pickle.dump(models_dict, open('model.pkl', 'wb'))
+    X_test.to_csv('x_test.csv', index=False)
+    y_test.to_csv('y_test.csv', index=False)
 
 
 
